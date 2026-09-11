62c5733e933a41716eaa43372e4363c93998e9ad
Rio <122155515+Rio-iOS@users.noreply.github.com>
2026-09-11 21:13:31 +0900

    Improve Swift API naming and access control in samples

--- rxswift-research-reading-book2/chapter01/Sources/chapter01/chapter01.swift
+++ rxswift-research-reading-book2/chapter01/Sources/chapter01/chapter01.swift
@@ -1,12 +1,12 @@
 import RxSwift
 
 @main
-public struct chapter01 {
+public struct Chapter01 {
     // 異常終了
     public enum TestError: Error {
         case test
     }
-    
+
     public static func main() {
         /*
          Rxではストリームにエラーイベントが通知されると、異常終了によりストリームは破棄され終了する。
@@ -35,16 +35,16 @@ public struct chapter01 {
                 print("flatMap: \(string)")
                 let observable = Observable<String>.create { observer in
                     observer.onNext("A")
-                    
+
                     observer.onCompleted()
-                    
+
                     return Disposables.create() {
                         print("Dispose Action.")
                     }
                 }
                 return observable
             }
-        
+
         _ = sequence.subscribe(onNext: {
             print("onNext: \($0)")
         }, onError: {
@@ -54,12 +54,12 @@ public struct chapter01 {
         }, onDisposed: {
             print("onDisposed:")
         })
-        
+
         /*
          正常終了の結果と違う部分は、Observable.of(1, 2)ストリームにおけるイベント「2」が発火していないこと
          flatMapにより作成されたObservable<String>ストリームの異常終了により、
          Observable.of(1, 2)のストリームの購読を停止させられた
-         
+
          エラーが通知されること自体は起こり得ることだが、
          それによってUIからのイベントを無視してしまうというのは想定外のはずであり、
          そのために正しいエラーハンドリングのための知識が重要になってくる
@@ -68,19 +68,19 @@ public struct chapter01 {
             print("flatMap: \(string)")
             let observable = Observable<String>.create { observer in
                 observer.onNext("A")
-                
+
                 observer.onError(TestError.test)
-                
+
                 observer.onNext("B")
-                
+
                 return Disposables.create() {
                     print("Dispose Action")
                 }
             }
-            
+
             return observable
         }
-        
+
         _ = sequence2.subscribe(onNext: {
             print("onNext: \($0)")
         }, onError: {
@@ -90,13 +90,13 @@ public struct chapter01 {
         }, onDisposed: {
             print("onDisposed:")
         })
-        
+
         // 購読の破棄
         /*
          disposeされると、その後の「B」イベントは購読されなくなる。
          */
         let subject = PublishSubject<String>()
-        
+
         let disposable = subject
             .subscribe(onNext: {
                 print("onNext: \($0)")
@@ -107,13 +107,13 @@ public struct chapter01 {
             }, onDisposed: {
                 print("onDisposed:")
             })
-        
+
         subject.onNext("A")
-        
+
         disposable.dispose()
-        
+
         subject.onNext("B")
-        
+
         subject.onCompleted()
     }
 }
